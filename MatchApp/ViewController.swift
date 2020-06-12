a40bad05e6e0b727d3c390c54f5178ce56ac4603
Igor <pastoreli.igor@gmail.com>
2020-06-12 10:56:40 -0300

    Initial Commit

---
+++ MatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MatchApp
+//
+//  Created by Igor Pastoreli on 6/12/20.
+//  Copyright © 2020 Igor Pastoreli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
